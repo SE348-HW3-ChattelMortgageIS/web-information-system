276ab8ed316739a1b2e404479e10507f8895943e
SHINNYCHOU <1437947307@qq.com>
2019-11-13 22:20:01 +0800

    front home

---
+++ front-end/front/src/view/home.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <left-navi></left-navi>
+  <!--<a>aaa</a>-->
+</div>
+</template>
+
+<script>
+  import leftNavi from '../components/leftNavi'
+
+    export default {
+        name: "home",
+        components: {leftNavi}
+    }
+</script>
+
+<style scoped>
+
+</style>
