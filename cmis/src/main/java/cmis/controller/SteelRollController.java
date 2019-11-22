65bf43c1ac652af3f60b4254c8375b8553c58075
Wonderful23 <2596409301@qq.com>
2019-11-22 11:32:32 +0800

    修复了一些bug

--- cmis/src/main/java/cmis/controller/SteelRollController.java
+++ cmis/src/main/java/cmis/controller/SteelRollController.java
@@ -4,6 +4,7 @@ import cmis.dto.CreateSteelRollRequest;
 import cmis.entity.SteelRoll;
 import cmis.entity.UserEntity;
 import cmis.service.SteelRollService;
+import cmis.service.getMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -29,27 +30,34 @@ public class SteelRollController {
 
     @Autowired
     private SteelRollService steelRollService;
-    @RequestMapping(value = "/findAllSteelRoll", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @Autowired
+    private getMessageService getMessageService1;
+    @RequestMapping(value = "/api/bank/findAllSteelRoll", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     @ResponseBody
     public List<SteelRoll> findAllSteelRoll() {
         return this.steelRollService.queryAllSteelRoll();
     }
-    @RequestMapping(value = "/deleteSteelRoll", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @RequestMapping(value = "/api/bank/deleteSteelRoll", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     @ResponseBody
     GeneralMessage delteSteelRoll(int id) {
         return steelRollService.delete(id);
     }
-    @RequestMapping(value = "/verifySteelRoll", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @RequestMapping(value = "/api/bank/verifySteelRoll", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     @ResponseBody
     GeneralMessage verifySteelRoll(int id) {
         return steelRollService.verify(id);
     }
-    @RequestMapping(value = "/createSteelRoll", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    @RequestMapping(value = "api/customer/createSteelRoll", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     @ResponseBody
     GeneralMessage createSteelRoll(@RequestBody CreateSteelRollRequest createSteelRollRequest) {
         User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         BigDecimal price = createSteelRollRequest.getPrice();
         String remark = createSteelRollRequest.getRemark();
         return this.steelRollService.create(price,remark,principal);
+    }
+    @RequestMapping(value = "/test", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    GeneralMessage test(String id) {
+        return getMessageService1.getStatus(id);
     }
 }
